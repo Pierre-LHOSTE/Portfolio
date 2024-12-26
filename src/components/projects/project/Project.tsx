01676ed2ea93fb8ba3b12850689659b2f7450a11
Pierre LHOSTE <vingt-douze@protonmail.com>
2024-12-26 18:01:44 +0100

    feat: enhance Project component to display selected stack icons

--- src/components/projects/project/Project.tsx
+++ src/components/projects/project/Project.tsx
@@ -2,6 +2,8 @@ import Button from "@/components/button/Button";
 import Tag from "@/components/tag/Tag";
 import type { ProjectType } from "../project";
 import "./project.scss";
+import { stackItem } from "@/components/stack/list";
+import Image from "next/image";
 import Album from "../album/Album";
 
 const IMG_MAX_WIDTH = 512;
@@ -11,11 +13,28 @@ export default function Project({
 }: {
   project: ProjectType;
 }) {
+  const selectedStacks = ["nextJs", "typescript", "javascript", "prisma"];
+
   return (
     <div className="project">
       <div>
         <header>
-          <h4>{project.name}</h4>
+          <h4>
+            {project.name}
+            <span>
+              {project.technologies
+                .filter((tech) => selectedStacks.includes(tech))
+                .map((tech) => (
+                  <Image
+                    key={tech}
+                    src={stackItem[tech].styles.icon}
+                    width={16}
+                    height={16}
+                    alt=""
+                  />
+                ))}
+            </span>
+          </h4>
           <Album images={[...new Set([project.thumbnail, ...project.images])]} />
         </header>
         <div className="content">
