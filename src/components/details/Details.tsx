ed5cd9230e79f1cbd84468f3464ae7844d501e49
Pierre LHOSTE <vingt-douze@protonmail.com>
2025-01-13 20:12:07 +0100

    feat: add download functionality for curriculum vitae

--- src/components/details/Details.tsx
+++ src/components/details/Details.tsx
@@ -5,6 +5,9 @@ import "./details.scss";
 import { useI18nContext } from "@/i18n/i18n-react";
 import { motion } from "motion/react";
 
+const DOWNLOAD_LINK =
+  "https://qg007gk6n2popinf.public.blob.vercel-storage.com/cv/pierre-lhoste-13-01-2025-rWO996UsK1mHU9GLq1ZFabKO57gxQ0.pdf";
+
 const variants = {
   hidden: {
     opacity: 0,
@@ -32,6 +35,10 @@ const animation = {
 export default function Details() {
   const { LL } = useI18nContext();
 
+  function downloadFile() {
+    window.open(DOWNLOAD_LINK, "_blank");
+  }
+
   return (
     <section id="details">
       <motion.p {...animation}>
@@ -39,7 +46,12 @@ export default function Details() {
       </motion.p>
       <motion.p {...animation}>
         {LL.hero.details.download.p1()}{" "}
-        <Button Icon={IconDownload} text={LL.hero.details.download.button()} type="secondary" />{" "}
+        <Button
+          Icon={IconDownload}
+          text={LL.hero.details.download.button()}
+          type="secondary"
+          onClick={downloadFile}
+        />{" "}
         {LL.hero.details.download.p2()}
       </motion.p>
     </section>
