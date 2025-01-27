5ddba70e9a1184cc54b8fdccaeae4c8f3453b3bf
Pierre LHOSTE <vingt-douze@protonmail.com>
2025-01-27 17:42:09 +0100

    fix: update addUser API

--- src/app/api/addUser/route.ts
+++ src/app/api/addUser/route.ts
@@ -1,25 +1,26 @@
 import { Client } from "@neondatabase/serverless";
 import { NextResponse } from "next/server";
 
-export async function POST() {
-  let client: Client | null = null;
+export async function POST(request: Request) {
+  const client = new Client({ connectionString: process.env.DATABASE_URL });
+  const { userAgent, ip, isBot } = await request.json();
+  if (!userAgent || !ip || typeof isBot !== "boolean") {
+    return NextResponse.json({ success: false, error: "Invalid data" }, { status: 400 });
+  }
 
   try {
-    client = new Client({ connectionString: process.env.DATABASE_URL });
     await client.connect();
-
     const today = new Date().toISOString().split("T")[0];
     const query = `
-      INSERT INTO visits (date, count)
-      VALUES ($1, 1)
-      ON CONFLICT (date)
-      DO UPDATE SET count = visits.count + 1
+      INSERT INTO users (user_agent, ip, date, is_bot, count)
+      VALUES ($1, $2, NOW(), $3, 1)
+      ON CONFLICT (ip)
+      DO UPDATE SET count = users.count + 1
     `;
-    await client.query(query, [today]);
-
+    await client.query(query, [userAgent, ip, isBot]);
     return NextResponse.json({ success: true }, { status: 200 });
   } catch (error) {
-    console.error("Error incrementing visit count:", error);
+    console.error("Error adding user:", error);
     return NextResponse.json({ success: false }, { status: 500 });
   } finally {
     if (client) {
